69292a4c3565971c19cd5855b567f6e36054a2e8
carloshee <carloshee@naver.com>
2017-07-06 17:36:10 +0900

    ...

---
+++ engine/mygdiplus_game.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+namespace myGdiplusGame {
+
+	const int g_nTileSize = 16;
+	const int g_nTileXCount = 8;
+
+	void drawTile(Graphics *pGrp, Image *pImgBasicTile, int nPosX, int nPosY, int *pMap)
+	{
+		int nTileIndex = pMap[nPosX + nPosY*g_nTileXCount];
+
+		pGrp->DrawImage(pImgBasicTile,
+			Rect(nPosX * 16, nPosY * 16, g_nTileSize, g_nTileSize),
+			g_nTileSize * (nTileIndex % g_nTileXCount), //¿øº» x À§Ä¡
+			g_nTileSize * (nTileIndex / g_nTileXCount),  //¿øº» y À§Ä¡
+			g_nTileSize, g_nTileSize,
+			UnitPixel);
+	}
+
+}
