a0a8937085fed1e3204624c835e30d0a8c79a8e4
carloshee <carloshee@naver.com>
2017-07-14 17:30:08 +0900

    총알 피하기 게임

---
+++ day18/exam4/game.h
@@ -0,0 +1,9 @@
+#pragma once
+
+
+extern void OnCreate(HWND hWnd);
+extern void OnDestory(HWND hWnd);
+extern S_ObjectPlayer g_objPlayer;
+extern BYTE g_KeyStatus[256];
+
+extern int AddEffectBulletDie(irr::core::vector2df pos);
