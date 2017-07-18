835ba15b322bd941538334cc82a625c662ba5bf6
carloshee <carloshee@naver.com>
2017-07-18 16:30:04 +0900

    총알피하기 게임

--- day18/exam4/ObjectPlayer.cpp
+++ day18/exam4/ObjectPlayer.cpp
@@ -2,23 +2,28 @@
 
 void S_ObjectPlayer_OnApply(S_ObjectPlayer *pThis, double fDelta)
 {
+	int i = 50;
+	if (g_KeyStatus['A']) {
+		i = 200;
+	}
 	if (g_KeyStatus[VK_LEFT]) {
 		if(pThis->m_vPosition.X > -300)
-			pThis->m_vPosition += fDelta * 50 * irr::core::vector2df(-1, 0);
+			pThis->m_vPosition += fDelta * i * irr::core::vector2df(-1, 0);
 	}
 	if (g_KeyStatus[VK_RIGHT]) {
 		if (pThis->m_vPosition.X < 300)
-			pThis->m_vPosition += fDelta * 50 * irr::core::vector2df(1, 0);
+			pThis->m_vPosition += fDelta * i * irr::core::vector2df(1, 0);
 	}
 
 	if (g_KeyStatus[VK_UP]) {
 		if (pThis->m_vPosition.Y > -220)
-			pThis->m_vPosition += fDelta * 50 * irr::core::vector2df(0, -1);
+			pThis->m_vPosition += fDelta * i * irr::core::vector2df(0, -1);
 	}
 	if (g_KeyStatus[VK_DOWN]) {
 		if (pThis->m_vPosition.Y < 220)
-			pThis->m_vPosition += fDelta * 50 * irr::core::vector2df(0, 1);
+			pThis->m_vPosition += fDelta * i * irr::core::vector2df(0, 1);
 	}
+	i = 50;
 }
 
 void S_ObjectPlayer_OnRender(S_ObjectPlayer *pThis, Graphics *pGrp)
