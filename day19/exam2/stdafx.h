c68fbfb023677d1a213b113a9e7cb880bb238444
carloshee <carloshee@naver.com>
2017-07-18 16:24:54 +0900

    ...

---
+++ day19/exam2/stdafx.h
@@ -0,0 +1,15 @@
+// 
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+
+// TODO: 프로그램에 필요한 추가 헤더는 여기에서 참조합니다.
+#include <WinSock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
